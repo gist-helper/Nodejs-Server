72430449659e1a09fabc83d2838538b67d35f9eb
hyoinandout <hobbyhyoin@naver.com>
2022-11-19 17:26:14 +0900

    feat: parsing extention to entire sheet

--- meal.py
+++ meal.py
@@ -9,3 +9,9 @@ class MealWrapper:
 
     def __init__(self) -> None:
         self.meal = Meal()
+
+kind_of_restaurants = ["제1학생회관1층","제1학생회관2층","제2학생회관1층"]
+kind_of_meals = ["조식","중식","삭식"]
+
+slot_endpoints = [(3,12),(13,22),(23,20)]
+slot_filenames_postfix = ['_b_kor.json','_l_kor.json','_d_kor.json']
--- resturants.py
+++ resturants.py
@@ -1,39 +1,33 @@
 import openpyxl
-import codecs
 import json
 
-from meal import MealWrapper
+from meal import MealWrapper, kind_of_meals, slot_endpoints, slot_filenames_postfix
 
-from utils import ComplexEncoder, sanitize_menu
+from util import ComplexEncoder, sanitize_menu
 
 # 1학생회관 1층 : 11월3째주메뉴.xlsx
 # 1학생회관 2층 : 주간식단표(2022년11월21일~2022년11월25일식단표).xlsx
 # 2학생회관 1층 : 11월3주.xlsx
-excel_path = "./2학생회관.xlsx"
+file_path = "./2학생회관.xlsx"
 
-wb = openpyxl.load_workbook(excel_path)
-sh = wb.worksheets[0]
+workbook = openpyxl.load_workbook(file_path)
+sheet = workbook.worksheets[0]
 
-for j in range(3):
-    jsonFile = open(f"./{j}.json", 'w+',encoding='utf-8')
+for column_day in range(4,11):
+    for row_slot in range(3):
+        day = sheet.cell(row=2, column=column_day).value
 
-    breakfast = MealWrapper()
-    
-    breakfast.meal.title="제2학생회관1층"
+        menus = ""
+        for row_menus in range(slot_endpoints[row_slot][0], slot_endpoints[row_slot][1]):
+            menus += sanitize_menu((sheet.cell(row=row_menus, column=column_day).value)) + '\n'
+        menus += sanitize_menu((sheet.cell(row=slot_endpoints[row_slot][1], column=column_day).value))
 
-    mealDate = sh.cell(row=2, column=4).value
-    
-    breakfast.meal.meal_date = mealDate.strftime('%Y-%m-%d')
-    #breakfast.meal.meal_date="2015-03-14"
-    
-    breakfast.meal.kind_of_meal = "조식"
-    
-    menus = ""
+        meal_wrapper = MealWrapper()
+        meal_wrapper.meal.title="제2학생회관1층"
+        meal_wrapper.meal.meal_date = day.strftime('%Y-%m-%d')
+        meal_wrapper.meal.kind_of_meal = kind_of_meals[row_slot]
+        meal_wrapper.meal.menu = menus
 
-    for k in range(3, 13):
-        menus += sanitize_menu((sh.cell(row=k, column=4).value)) + '\n'
-    #breakfast.meal.menu="콩밥"
-
-    breakfast.meal.menu=menus
-
-    json.dump(breakfast.__dict__,jsonFile,indent=4,ensure_ascii=False,cls=ComplexEncoder)
+        default_name = day.strftime('%m_$d') + slot_filenames_postfix[row_slot]
+        jsonFile = open(f"./{default_name}.json", 'w+',encoding='utf-8')
+        json.dump(meal_wrapper.__dict__,jsonFile,indent=4,ensure_ascii=False,cls=ComplexEncoder)
